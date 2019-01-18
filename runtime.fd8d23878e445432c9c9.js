933531a7dbaf672552b7ccf18fe3e6b2db2fc684
alexpozdnyakof <alexpozdnyakof@gmail.com>
2019-01-18 20:55:34 +0300

    config module added

--- runtime.fd8d23878e445432c9c9.js
+++ runtime.fd8d23878e445432c9c9.js
@@ -63,7 +63,7 @@
 /******/
 /******/ 	// script path function
 /******/ 	function jsonpScriptSrc(chunkId) {
-/******/ 		return __webpack_require__.p + "" + ({}[chunkId]||chunkId) + "." + {"0":"7f195bee841062110e22","1":"c15143b6d44cf92ac242"}[chunkId] + ".js"
+/******/ 		return __webpack_require__.p + "" + ({}[chunkId]||chunkId) + "." + {"0":"7f195bee841062110e22","1":"a46266317991f55941e4"}[chunkId] + ".js"
 /******/ 	}
 /******/
 /******/ 	// The require function
