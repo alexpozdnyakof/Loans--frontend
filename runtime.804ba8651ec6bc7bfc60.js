2754e700051754a6687a937a1c4155ab3c9a3818
alexpozdnyakof <alexpozdnyakof@gmail.com>
2019-02-14 16:48:03 +0300

    layout fix
    previous value switch control moved to top

--- runtime.804ba8651ec6bc7bfc60.js
+++ runtime.804ba8651ec6bc7bfc60.js
@@ -63,7 +63,7 @@
 /******/
 /******/ 	// script path function
 /******/ 	function jsonpScriptSrc(chunkId) {
-/******/ 		return __webpack_require__.p + "" + ({}[chunkId]||chunkId) + "." + {"0":"7f195bee841062110e22","1":"87e80251a66acbdb5d1f"}[chunkId] + ".js"
+/******/ 		return __webpack_require__.p + "" + ({}[chunkId]||chunkId) + "." + {"0":"7f195bee841062110e22","1":"9adfd57019198de12bc8"}[chunkId] + ".js"
 /******/ 	}
 /******/
 /******/ 	// The require function
